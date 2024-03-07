ca2dbb352cf7ae6a73dc0e59b6238e5eb60b5240
shubham <shubhamnainwal70@gmail.com>
2024-03-08 00:45:33 +0530

    final

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from "./pages/Signup.tsx";
 import Signin from "./pages/Signin.tsx";
-import Blog from "./pages/Blog.tsx";
+import { Blog } from "./pages/Blog.tsx";
+import { Blogs } from "./pages/Blogs.tsx";
 import "./App.css";
+import { Publish } from "./pages/Publish.tsx";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/publish" element={<Publish />} />
         </Routes>
       </BrowserRouter>
     </>
